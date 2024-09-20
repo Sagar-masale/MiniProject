84aa46cc7ca7484667a4519d706ec8f328d59906
Sagar-masale <sagarmasale27@gmail.com>
2024-09-20 20:41:42 +0530

    add files

---
+++ script.js
@@ -0,0 +1,21 @@
+const audio1 = document.getElementById("playContinueSong");
+const audio2 = document.getElementById("playEndSong");
+
+function playContinueSong() {
+  audio2.pause();
+    if (audio1.paused) {
+      audio1.play();
+    } else {
+      audio1.pause();
+    }
+}
+
+function endSong() {
+  audio1.pause();
+  audio1.currentTime = 0;
+  if (audio2.paused) {
+    audio2.play();
+  } else {
+    audio2.pause();
+  }
+}
